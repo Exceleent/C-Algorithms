e098a59fa3595e4d92c6426780abbd6e76bf16fc
MichalZnalezniak <42517471+Exceleent@users.noreply.github.com>
2019-03-02 18:11:30 +0100

    Add files via upload

---
+++ Graphs/BinaryTree.h
@@ -0,0 +1,28 @@
+
+
+
+class BinaryTree {
+    public:
+    struct Root {
+        int value;
+        Root* left_child;
+        Root* right_child;
+        Root* parent;
+    };
+ Root* root;
+ int size_of_Tree;
+ BinaryTree();
+ void insert(int x); 
+ Root* searchRecursive(int x); 
+ bool searchIterative(int x); 
+ Root* searchIterative_TRO(int x );
+int size(); 
+int minimum();  
+int maximum();  
+int depth(struct Root *node);  
+void printInorder(struct Root* node);  
+void printPreorder(struct Root* node); 
+void printPostorder(struct Root* node); 
+void Print_iterative();
+
+};
